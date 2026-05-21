f4a9ba87994e09a7cf3845b36d743a977ba41153
Matthew32342 <Matthewebgeorge@gmail.com>
2026-05-22 09:45:24 +1200

    Finished page transtion no lag vr 22/05/26

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
 // PAGE TRANSITION
 const links = document.querySelectorAll(".transition-link");
+
 const transition = document.querySelector(".page-transition");
 
 links.forEach(link => {
+
   link.addEventListener("click", function (e) {
+
     e.preventDefault();
 
     const target = this.href;
 
+    // START BLUE SLIDE
     transition.classList.add("active");
 
+    // WAIT BEFORE CHANGING PAGE
     setTimeout(() => {
+
       window.location.href = target;
-    }, 600);
+
+    }, 400);
+
   });
+
 });
 
+// RESET ANIMATION ON LOAD
 window.addEventListener("pageshow", () => {
+
   transition.classList.remove("active");
-  transition.classList.add("exit");
 
-  setTimeout(() => {
-    transition.classList.remove("exit");
-  }, 700);
 });
-
 
 // SCROLL ANIMATION
 const observer = new IntersectionObserver((entries) => {
+
   entries.forEach((entry) => {
 
     if (entry.isIntersecting) {
+
       entry.target.classList.add("show");
-    } 
-    
-    else {
+
+    } else {
+
       entry.target.classList.remove("show");
+
     }
 
   });
+
+}, {
+  threshold: 0.15
 });
 
 const hiddenElements = document.querySelectorAll(
